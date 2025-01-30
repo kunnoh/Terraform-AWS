d0bc8324d91b81fb7f76048069a19851e5bd9cf1
kunnoh <alvindereba@gmail.com>
2025-01-30 17:26:41 +0300

    added shadowsocks udp sg

--- main.tf
+++ main.tf
@@ -152,6 +152,22 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_shadowsocks" {
+  security_group_id = aws_security_group.allow_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8022
+  ip_protocol       = "tcp"
+  to_port           = 8022
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_shadowsocks_udp" {
+  security_group_id = aws_security_group.allow_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8022
+  ip_protocol       = "udp"
+  to_port           = 8022
+}
+
 # Egress all any ports
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_traffic.id
